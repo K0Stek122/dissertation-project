c68632d9a42efc765aee39fee04c22225636953a
K0Stek122 <kamilianos3@gmail.com>
2026-02-18 10:46:10 +0000

    refactor: separated variables and mehods in classes

--- src/PacketCapture.h
+++ src/PacketCapture.h
@@ -12,17 +12,19 @@
 #include "PacketFilter.h"
 
 class PacketCapture {
+// VARIABLES
+private:
+    std::optional<PacketFilter> packet_filter;
+    std::deque<PacketEvent> packet_backlog;
+
+// METHODS
 private:
     PacketEvent cast_packet(pcpp::RawPacket* packet);
     std::string get_tcp_flags(pcpp::TcpLayer* tcpLayer);
-    std::optional<PacketFilter> packet_filter;
-    std::deque<PacketEvent> packet_backlog;
-    
     bool matches_pattern(const PacketEvent& e, const PacketFilter& f);
 
 public:
     std::optional<PacketEvent> capture(pcpp::RawPacket* raw_packet);
     int add_filter(PacketFilter filter_type);
-    
     bool process_packet_backlog();
 };
