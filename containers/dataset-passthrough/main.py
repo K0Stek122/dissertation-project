eb0f0f8eb34b389a9086cc7e678e3fe18e8ead09
K0Stek122 <kamilianos3@gmail.com>
2026-03-31 09:13:33 +0100

    feat: implemented argument parsing

---
+++ containers/dataset-passthrough/main.py
@@ -0,0 +1,30 @@
+import argparse
+
+
+def setup_arguments():
+    parser = argparse.ArgumentParser(description="Dataset Passthrough Container")
+    
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        required=True,
+        help="Input dataset path."
+    )
+    
+    # Argument that accepts a list of possible options
+    parser.add_argument(
+        "-d",
+        "--dataset-type",
+        type=str,
+        choices=["UNSW-NB15", "CICIDS2017", "CAIDA2007"],
+        required=True,
+        help="Operation to perform on the dataset."
+    )
+    
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = setup_arguments()
+    
+    
